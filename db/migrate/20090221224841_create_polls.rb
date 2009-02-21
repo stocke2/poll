9cd31afe7e169c1ee7379779cfd38a1c3d05e252
Eric Stockbridge <stocke2@gmail.com>
2009-02-21 16:50:42 -0600

    add poll model

---
+++ db/migrate/20090221224841_create_polls.rb
@@ -0,0 +1,12 @@
+class CreatePolls < ActiveRecord::Migration
+  def self.up
+    create_table :polls do |t|
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :polls
+  end
+end
