3fa983130c63a5f259113d8fc7e7f8794f5b0eaf
Eric Stockbridge <stocke2@gmail.com>
2009-02-23 08:44:02 -0600

    fixed up presentation a bit

--- app/helpers/poll_helper.rb
+++ app/helpers/poll_helper.rb
@@ -1,7 +1,7 @@
 module PollHelper
   def add_answer_link(name) 
     link_to_function name do |page| 
-      page.insert_html :bottom, :answers, :partial => 'answer', :object => Answer.new 
+      page.insert_html :bottom, :answers_list, :partial => 'answer', :object => Answer.new 
     end 
   end 
 end
