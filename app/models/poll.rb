f66804d5d6640c178af4be9fbf722e5decc2e375
Eric Stockbridge <stocke2@gmail.com>
2009-02-23 22:35:57 -0600

    validate length of poll title

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,7 +2,7 @@ class Poll < ActiveRecord::Base
   has_many :answers, :dependent => :destroy
   
   validates_presence_of :title
-  
+  validates_length_of :title, :maximum => 50
   after_update :save_answers
   
   def new_answer_attributes=(answer_attributes)
